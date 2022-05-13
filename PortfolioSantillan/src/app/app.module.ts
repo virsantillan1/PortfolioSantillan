deba6e29280463752fb4c93e99c83300ca08e3c6
virsantillan1 <virsantillan98@hotmail.com.ar>
2022-05-13 16:43:33 -0300

    Components: header, socialmedia, banner

--- PortfolioSantillan/src/app/app.module.ts
+++ PortfolioSantillan/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProComponent } from './components/logo-arg-pro/logo-arg-pro.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProComponent,
+    SocialmediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
